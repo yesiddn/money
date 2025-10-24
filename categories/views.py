480dfdee60ea9f5fcb4bd5ab271f92e4ed3a5fa7
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-10-23 22:22:57 -0500

    Add CategoryViewSet with authentication and user-scoped queryset

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.categories.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
