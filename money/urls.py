f0801b22f37d24ed731f1cbc3a35fbd5e9a34482
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-10-23 22:24:41 -0500

    Add categories URL router and include it in project URLs

--- money/urls.py
+++ money/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -29,4 +29,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("api/token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("api/categories/", include("categories.urls")),
 ]
