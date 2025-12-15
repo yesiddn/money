acbab9ecf87626a466b89c3b335f9a43a8ce9307
Duvan Yesid <yesidrodriguez305@gmail.com>
2025-12-14 22:32:27 -0500

    Update currency field in Account model to use default currency and enforce protection on deletion

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def get_default_currency():
+    from currencies.models import Currency
+
+    return Currency.objects.get(code="COP").code
+
+
 class Account(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     currency = models.ForeignKey(
-        "currencies.Currency", on_delete=models.SET_NULL, null=True, blank=True
+        "currencies.Currency", on_delete=models.PROTECT, default=get_default_currency
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
